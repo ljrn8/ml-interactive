8b468bfa6b7ffcc6cec80510790096a81a6d4a03
Joel Hoefs <joel.hoefs7@gmail.com>
2023-01-20 22:25:08 +0800

    added title bar

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,24 @@
+import Title from "./components/Title";
+import flower from './assets/flower.png';
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+
+      <Title image={flower}/>
+
+      <nav>
+        <ul className="nav-list">
+          <li className='nav-link'>Softmax</li>
+          <li className='nav-link'>SVM</li>
+          <li className='nav-link'>Random Forest</li>
+        </ul>
+      </nav>
+
+
+
+    </div>
+  );
 }
 
 export default App;
---
+++ app/src/components/Title.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Title = ({ image }) => {
+  return (
+    <>
+      <img src={image} alt='...' id='title-logo'></img> 
+      <img src={image} alt='...' id='title-logo-2'></img> 
+
+      <header className='title'>
+        <h1 style={{ fontSize: '2.5rem' }}> Interactive ML Classifiers </h1>
+        <p> A breif look at some simple classifier algorithms </p>
+      </header>
+    </>
+  );
+};
+
+
+export default Title;
